af0b53282d16d3339217bcec739ac3d76f04f10a
Corentin Henry <corentinhenry@gmail.com>
2019-06-23 12:45:40 +0200

    add a custom filter
    (selmer/render
        "{% if files|empty? %}no files{% else %}files{% endif %}"
        {:files []})
    
    ;; => "no files"

--- src/html_templating/core.clj
+++ src/html_templating/core.clj
@@ -1,2 +1,8 @@
 (ns html-templating.core
-  (:require [selmer.parser :as selmer]))
+  (:require [selmer.parser :as selmer]
+            [selmer.filters :as filters]))
+
+;; (let [template "{% if files|empty? %}no files{% else %}files{% endif %}"]
+;;     (selmer/render template {:files []}))
+;; => "no files"
+(filters/add-filter! :empty? empty?)
