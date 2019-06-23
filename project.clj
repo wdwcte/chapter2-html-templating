c21c90754aea9f10a5acf1230c5e00c46d235e9f
Corentin Henry <corentinhenry@gmail.com>
2019-06-23 10:47:57 +0200

    use cider-nrepl plugin

--- project.clj
+++ project.clj
@@ -3,5 +3,6 @@
   :url "http://example.com/FIXME"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
+  :plugins [[cider/cider-nrepl "0.22.0-beta4"]]
   :dependencies [[org.clojure/clojure "1.10.0"]]
   :repl-options {:init-ns html-templating.core})
